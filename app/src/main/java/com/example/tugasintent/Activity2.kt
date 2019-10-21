8198deb25a316e9f46be0a70c15b63947529cb06
creepyif <ipung.fachrizal@gmail.com>
2019-10-21 10:46:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugasintent/Activity2.kt
@@ -0,0 +1,49 @@
+package com.example.tugasintent
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity2.*
+
+
+class Activity2 : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity2)
+
+        val bundle= intent.extras
+        val nim2 = bundle?.get("nim")
+        val nama2 = bundle?.get("nama")
+        val nilai2 = bundle?.get("nilai")
+        var nilai = nilai2.toString()
+        var nilaib = nilai.toInt()
+
+        if (nilaib >= 80){
+            keterangan.setText("A")
+        }
+        else if(nilaib >= 60){
+            keterangan.setText("B")
+        }
+        else if(nilaib >= 40){
+            keterangan.setText("C")
+        }
+        else if(nilaib >= 20){
+            keterangan.setText("D")
+        }
+        else{
+            keterangan.setText("E")
+        }
+        nim_hasil.setText(""+nim2)
+        nama_hasil.setText(""+nama2)
+        nilai_hasil.setText(""+nilai2)
+
+        button.setOnClickListener(){
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/tugasintent/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.tugasintent
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        var nim1: EditText = findViewById(R.id.nim)
+        var nama1: EditText = findViewById(R.id.nama)
+        var nilai1: EditText = findViewById(R.id.nilai)
+
+
+        btn_proses.setOnClickListener{
+
+            intent = Intent(this, Activity2::class.java)
+            intent.putExtra("nim",nim1.getText())
+            intent.putExtra("nama",nama1.getText())
+            intent.putExtra("nilai",nilai1.getText())
+
+            startActivity(intent)
+        }
+
+    }
+}
